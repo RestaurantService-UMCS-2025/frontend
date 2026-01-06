a3d9c4ae835c4cd8acfbcd7bd35827a66a984b2c
xxylu <oliwierpachulski@gmail.com>
2026-01-06 16:00:34 +0100

    feat: enums added

---
+++ src/models/enums/OrderItemStatus.ts
@@ -0,0 +1,6 @@
+export enum OrderItemStatus {
+    Ordered,
+    Preparing,
+    Ready,
+    Delivered,
+}
---
+++ src/models/enums/OrderStage.ts
@@ -0,0 +1,6 @@
+export const enum OrderStage {
+    Filled,
+    AwaitingPayment,
+    Paid,
+    NULL
+}
