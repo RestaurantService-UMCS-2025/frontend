111a946f5d7e99324fbee82dc4871f48e1f482e8
xxylu <oliwierpachulski@gmail.com>
2026-01-14 00:39:58 +0100

    api test view

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { CartProvider } from './context/CartContext';
-import MenuScreen from './screens/MenuScreen';
-import CartScreen from './screens/CartScreen';
+// import { CartProvider } from './context/CartContext';
+// import MenuScreen from './screens/MenuScreen';
+// import CartScreen from './screens/CartScreen';
+//
+// export default function App() {
+//     return (
+//         <CartProvider>
+//             {/* Twoja nawigacja lub komponenty */}
+//             <MenuScreen />
+//             <CartScreen />
+//         </CartProvider>
+//     );
+// }
 
-export default function App() {
+import { TablesTestView } from '../src/screens/TablesTestView';
+
+function App() {
     return (
-        <CartProvider>
-            {/* Twoja nawigacja lub komponenty */}
-            <MenuScreen />
-            <CartScreen />
-        </CartProvider>
+        <div className="App">
+            <TablesTestView />
+        </div>
     );
 }
+
+export default App;
